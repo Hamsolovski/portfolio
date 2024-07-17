7034141df6cff7381786e2e2ae835acbbb297c0f
Antoine <ant.delalande@gmail.com>
2024-07-17 15:08:28 +0200

    added grid for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
 import "./App.css";
 
-import Label from "./components/Label/Label"
+import Label from "./components/Label/Label";
 import ProjectCard from "./components/ProjectCard/ProjectCard";
 
 function App() {
@@ -11,37 +10,71 @@ function App() {
         <section className="portfolio__id">
           <h1>Antoine Delalande</h1>
           <p>Développeur web</p>
-        </section>
-        <section className="portfolio__who_am_i">
-          <h2>Qui suis-je ?</h2>
-          <p>Je suis un développeur web basé à Paris, en France. Après avoir travaillé dans la communication pour diverses associations environnementales pendant 8 ans, j’ai suivi une formation de développeur web à la Wild Code School, Paris.</p>
-          <p>Très intéressé par l’impact du numérique, j’aspire à faire du web un endroit plus humain et écologique.</p>
-          <p>J’ai deux chats, je dessine, et j’aime un peu trop les jeux et le game design.</p>
-        </section>
-        <section className="portfolio__skills">
-          <h2>Mes compétences</h2>
-          <h3>Langages</h3>
-          <Label />
-          {/* Faire un map ici */}
-          <h3>Frontend</h3>
-          <h3>Backend</h3>
-          <h3>Outils</h3>
+          <section className="portfolio__who_am_i">
+            <h2>Qui suis-je ?</h2>
+            <p>
+              Je suis un développeur web basé à Paris, en France. Après avoir
+              travaillé dans la communication pour diverses associations
+              environnementales pendant 8 ans, j’ai suivi une formation de
+              développeur web à la Wild Code School, Paris.
+            </p>
+            <p>
+              Très intéressé par l’impact du numérique, j’aspire à faire du web
+              un endroit plus humain et écologique.
+            </p>
+            <p>
+              J’ai deux chats, je dessine, et j’aime un peu trop les jeux et le
+              game design.
+            </p>
+          </section>
+          <section className="portfolio__skills">
+            <h2>Mes compétences</h2>
+            <h3>Langages</h3>
+            <Label />
+            <Label />
+            <Label />
+            <Label />
+            {/* Faire un map ici */}
+            <h3>Frontend</h3>
+            <Label />
+            <Label />
+            <h3>Backend</h3>
+            <Label />
+            <Label />
+            <Label />
+            <Label />
+            <Label />
+            <Label />
+            <h3>Outils</h3>
+            <Label />
+            <Label />
+            <Label />
+          </section>
         </section>
         <section className="portfolio__projects">
           <h2>Mes projets web</h2>
-          <ProjectCard/>
-          <ProjectCard/>
-          <ProjectCard/>
+          <section className="portfolio__projects_grid">
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+          </section>
         </section>
-        <footer className="portfolio__contact">
-          <ul>
-            <li>LinkedIn</li>
-            <li>Github</li>
-          </ul>
-          <a href="mailto:hamsolovski@gmail.com">Contactez-moi</a>
-        </footer>
       </main>
-      
+      <footer className="portfolio__contact">
+        <ul>
+          <li>LinkedIn</li>
+          <li>Github</li>
+        </ul>
+        <a href="mailto:hamsolovski@gmail.com">Contactez-moi</a>
+      </footer>
     </section>
   );
 }
--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,7 +1,13 @@
+import Label from "../Label/Label";
+
+
 function ProjectCard () {
     return (
         <article>
             <h1>Projet</h1>
+            <p>Avril 2024</p>
+            <p>Projet de formation - réseau social basique des élèves de ma promo</p>
+            <Label />
         </article>
     )
 }
