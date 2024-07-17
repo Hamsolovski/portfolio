8c4b61f0fea42f0dc70ef12e492b47da859be096
Antoine <ant.delalande@gmail.com>
2024-07-17 11:54:30 +0200

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <section className="portfolio__background">
+      <main className="portfolio__main_container">
+        <section className="portfolio__id">
+          <h1>Antoine Delalande</h1>
+          <p>Développeur web</p>
+        </section>
+        <section className="portfolio__who_am_i">
+
+        </section>
+        <section className="portfolio__skills">
+
+        </section>
+        <section className="portfolio__projects">
+
+        </section>
+        <footer className="portfolio__contact"></footer>
+      </main>
+      
+    </section>
+  );
+}
+
+export default App;
