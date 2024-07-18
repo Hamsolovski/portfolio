aaa4aa5c7b0b81ff8bff49ad82453a5fa6121954
Antoine <ant.delalande@gmail.com>
2024-07-18 15:42:31 +0200

    added links to grid

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,22 +1,25 @@
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
 import PropTypes from "prop-types";
 import "./projectCard.css";
 import Label from "../Label/Label";
 
 function ProjectCard({ project }) {
   return (
-    <article className={`project-card noise ${project.tag}`}>
-      <h2>{project.name}</h2>
-      <section>
-      <p className="project-card__time">{project.date}</p>
-      <p className="project-card__desc">{project.desc}</p>
-      <footer className="portfolio__skills_display">
-        {project.stack.map((language) => (
-          <Label key={language} label={language} />
-        ))}
-      </footer>
-      </section>
-      
-    </article>
+    <a href={project.link} className="project-card__link">
+      <article className={`project-card noise ${project.tag}`}>
+        <h2>{project.name}</h2>
+        <section>
+          <p className="project-card__time">{project.date}</p>
+          <p className="project-card__desc">{project.desc}</p>
+          <footer className="portfolio__skills_display">
+            {project.stack.map((language) => (
+              <Label key={language} label={language} />
+            ))}
+          </footer>
+          <FaRegArrowAltCircleRight className="project-card__link-icon" />
+        </section>
+      </article>
+    </a>
   );
 }
 
--- src/components/ProjectList/list.js
+++ src/components/ProjectList/list.js
@@ -1,45 +1,63 @@
 const projects = [
     {
-        id: 1,
-        name: "Moov'it",
-        date: "Juillet 2024",
-        desc: "Projet de formation - journal ludique pour démarrer et suivre son activité sportive",
+        id: 5,
+        name: "Portfolio",
+        date: "Juillet 2024 - Projet perso",
+        desc: "Le site sur lequel vous êtes actuellement ;)",
         stack: [
-            "Javascript", "React", "Express.js", "MySQL"  
+            "Javascript", "React"
         ],
-        tag: "moov-it"
-    },
-    {
-        id: 2,
-        name: "Out",
-        date: "Mai 2024",
-        desc: "Hackathon - un site pour inciter les utilisateurs à déconnecter et découvrir des activités près de chez eux",
-        stack: [
-            "Javascript", "React"  
-        ],
-        tag: "out"
-    },
-    {
-        id: 3,
-        name: "Wizard's Castle",
-        date: "Avril 2024",
-        desc: "Projet de formation - site d’aide pour les joueurs et joueuses de D&D",
-        stack: [
-            "Javascript", "React" 
-        ],
-        tag: "wizard-castle"
+        tag: "portfolio",
+        link: "https://hamsolovski.fr"
     },
     {
         id: 4,
+        name: "Moov'it",
+        date: "Juillet 2024 - Projet de formation",
+        desc: "Journal ludique pour démarrer et suivre son activité sportive",
+        stack: [
+            "Javascript", "React", "Express.js", "MySQL"  
+        ],
+        tag: "moov-it",
+        link: "https://moovit.hamsolovski.fr"
+    },
+    {
+        id: 3,
+        name: "Out",
+        date: "Mai 2024 - Hackathon",
+        desc: "Un site pour inciter les utilisateurs à déconnecter et découvrir des activités près de chez eux",
+        stack: [
+            "Javascript", "React"  
+        ],
+        tag: "out",
+        link: "https://out-omega-dun.vercel.app/"
+    },
+    {
+        id: 2,
+        name: "Wizard's Castle",
+        date: "Avril 2024 - Projet de formation",
+        desc: "Un site d’aide pour les joueurs et joueuses de D&D",
+        stack: [
+            "Javascript", "React" 
+        ],
+        tag: "wizard-castle",
+        link: "https://wcs-p2-wizard-castle.vercel.app"
+    },
+    {
+        id: 1,
         name: "Wild Social",
-        date: "Février 2024",
-        desc: "Projet de formation - réseau social basique des élèves de ma promo",
+        date: "Février 2024 - Projet de formation",
+        desc: "Un réseau social dédié aux Wilders, les membres de ma promo",
         stack: [
             "Javascript", "HTML5", "CSS3"
         ]
         ,
-        tag: "wild-social"
+        tag: "wild-social",
+        link: "https://hamsolovski.github.io/wild-social/"
+        
     },
+    
+    
 ]
 
 export default projects;
